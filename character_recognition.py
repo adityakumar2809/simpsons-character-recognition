51e6e9ccd11ab44e0b0341c27eeff30191f8e3f5
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-12 01:55:18 +0530

    Import dataset

--- character_recognition.py
+++ character_recognition.py
@@ -11,6 +11,13 @@ def main():
     channels = 1
     char_path = r'../input/the-simpsons-characters-dataset/simpsons_dataset'
 
+    char_dict = {}
+    for char in os.listdir(char_path):
+        char_dict[char] = len(os.listdir(os.path.join(char_path, char)))
+        
+    # Sort in descending order
+    char_dict = caer.sort_dict(char_dict, descending=True)
+
 
 if __name__ == "__main__":
     main()
